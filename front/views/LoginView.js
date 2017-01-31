bf1f0d47e8c38b0640ebc0f9d334f8c6e67b2eae
Ernesto Celis <developer@celisdelafuente.net>
2017-01-30 18:41:30 -0600

    ADD Login Backbone view

---
+++ front/views/LoginView.js
@@ -0,0 +1,26 @@
+'use strict'
+
+let LoginView = Backbone.View.extend({
+
+  events: {
+    "click #login-button": "tryLogin"
+  },
+
+  tryLogin: function() {
+
+    if(this.$("#login-form-1").parsley('validate')) {
+      console.log("Is valid!!");
+    } else {
+      console.log("Not valid :(");
+    }
+  },
+
+  render: function() {
+    let template = $("#login-template").html();
+    let compiled = _.template(template, { name: "Template Name" });
+    $(this.el).html(compiled);
+//    return this;
+    $(".content-element").html(this.el);
+  }
+
+});
