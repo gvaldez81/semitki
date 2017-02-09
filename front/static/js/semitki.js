ac6f98d4de0c0bf27bc1627c261a2f76e5bd2aef
betomtz <luis.mave88@gmail.com>
2017-02-09 17:21:05 -0600

    modify scheduler, fix all dising

--- front/static/js/semitki.js
+++ front/static/js/semitki.js
@@ -2,9 +2,9 @@ $(window).on('load', () => {
 
   // Initialize
   let posts = new Posts; // Collection first
-  loginView = new SchedulerCreate({collection: posts});
-  posts.fetch();
-  let semitkiRouter = new SemtikiRouter();
-  semitkiRouter();
-//  loginView.render({ el: "#container" });
+  loginView = new SchedulerView({collection: posts});
+//  posts.fetch();
+//  let semitkiRouter = new SemtikiRouter();
+//  semitkiRouter();
+ loginView.render({ el: "#container" });
 });
