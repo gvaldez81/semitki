0e2d5fb791ea07880a5ea67fe87a5059e7f5232e
Ernesto Celis <developer@celisdelafuente.net>
2017-02-09 13:54:20 -0600

    WIP Router almost working, it does not remove previous render though :/

--- front/static/js/semitki.js
+++ front/static/js/semitki.js
@@ -1,10 +1,27 @@
+'use strict'
+
+let Semitki = {
+
+  initialize() {
+    // Initialize
+    console.log("I'm heating up")
+    Backbone.history.start();
+    this.router = new SemitkiRouter();
+    // TODO Get CSRF cookie here
+/*    let posts = new Posts; // Collection first
+    loginView = new SchedulerCreate({collection: posts});
+    posts.fetch();
+    let semitkiRouter = new SemtikiRouter();
+    semitkiRouter();
+    */
+  },
+};
+
+
 $(window).on('load', () => {
 
-  // Initialize
-  let posts = new Posts; // Collection first
-  loginView = new SchedulerCreate({collection: posts});
-  posts.fetch();
-  let semitkiRouter = new SemtikiRouter();
-  semitkiRouter();
+  Semitki.initialize();
+  Semitki.router.navigate("scheduler", {trigger: true});
+
 //  loginView.render({ el: "#container" });
 });
