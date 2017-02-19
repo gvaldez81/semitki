4c0da42a98b5ac21d85a1defed332869d5c5782a
Ernesto Celis <developer@celisdelafuente.net>
2017-02-19 17:53:55 -0600

    I lapsed, but I think I was working on social networks plugins

--- front/static/js/semitki.js
+++ front/static/js/semitki.js
@@ -8,6 +8,7 @@ let Semitki = {
     this.router = new SemitkiRouter();
     this.jwtheader = "JWT ";
     this.jwtoken = undefined;
+    // BackBone collection instances
     this.collection = {
       "projects": new Projects(),
       "topics": new Topics()
@@ -29,13 +30,15 @@ let Semitki = {
 
 };
 
-
-$(window).on('load', () => {
+// Launch the JavaScript client side app
+$(() => {
 
   Semitki.initialize();
+  // Check for a valid JWToken and route the user accordingly
   if(Semitki.jwtoken == undefined) {
     Semitki.router.index();
   } else {
     Semitki.router.dashboard();
   }
+
 });
