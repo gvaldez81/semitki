e80f7dd07bdc3442d653a1c3f79cd1870535fed6
Ernesto Celis <developer@celisdelafuente.net>
2017-02-27 16:14:32 -0600

    UPDATE deployment to run migrate

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 cd /opt/semitki
 git pull
+cd api
+/opt/semitki/ENV/bin/python manage.py migrate
 cd front
 npm install
 bower install
 grunt
 cd ..
-docker-compose up -d
-#docker-compose up -d --build
+#docker-compose up -d
+docker-compose up -d --build
