7e8dc296dbe3ac9500efa03e41881f04610c8091
Ernesto Celis <developer@celisdelafuente.net>
2017-02-28 08:56:36 -0600

    FIX missing python modules when rebuilding the container

--- run_api.sh
+++ run_api.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd /semitki
+ENV/bin/pip install -r requirements.txt
 cd /semitki/api
 /semitki/ENV/bin/python /semitki/api/manage.py migrate
 /semitki/ENV/bin/python /semitki/api/manage.py runserver 0.0.0.0:8000
