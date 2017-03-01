d8de8e14ab40a00bafc1a1d26187ff698f73bd06
Ernesto Celis <developer@celisdelafuente.net>
2017-03-01 15:50:32 -0600

    ADD remarks

--- front/static/js/util.js
+++ front/static/js/util.js
@@ -1,9 +1,12 @@
 'use strict'
 
-//let api_url = "127.0.0.1";
-let api_url = "159.203.134.236";
+let api_url = "127.0.0.1";
+//let api_url = "159.203.134.236";
 let api_port = 8000;
 
+/**
+ * Initializie Facebook JavaScript SDK 
+ */
 window.fbAsyncInit = function() {
   FB.init({
           appId      : 'app_id',
@@ -14,21 +17,21 @@ window.fbAsyncInit = function() {
 };
 
 (function(d, s, id){
- var js, fjs = d.getElementsByTagName(s)[0];
- if (d.getElementById(id)) {return;}
- js = d.createElement(s); js.id = id;
- js.src = "//connect.facebook.net/en_US/sdk.js";
- fjs.parentNode.insertBefore(js, fjs);
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
 
 function sameOrigin(url) {
   // test that a given url is a same-origin URL
   // url could be relative or scheme relative or absolute
-  var host = document.location.host; // host + port
-  var protocol = document.location.protocol;
-  var sr_origin = '//' + host;
-  var origin = protocol + sr_origin;
+  let host = document.location.host; // host + port
+  let protocol = document.location.protocol;
+  let sr_origin = '//' + host;
+  let origin = protocol + sr_origin;
 
   // Allow absolute or scheme relative URLs to same origin
   return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
