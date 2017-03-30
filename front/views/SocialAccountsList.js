a642765f00c3752a4f2848194c1d1f17e5b60bf5
Ernesto Celis <developer@celisdelafuente.net>
2017-03-30 12:40:44 -0600

    ADD Social accounts subview

---
+++ front/views/SocialAccountsList.js
@@ -0,0 +1,23 @@
+'use strict'
+
+let SocialAccountsList = Backbone.View.extend({
+
+  tagName: "div",
+
+  className: "form-group",
+
+  render: function() {
+    let template = $("#accounts-list-template").html();
+    let compiled = Handlebars.compile(template);
+    let data = {
+      accounts: Semitki.collection.get("accounts").toJSON()
+    };
+
+    this.$el.html(compiled(data));
+    $("#accounts-list-container").append(this.$el);
+
+    // TODO FIX slect2 not rendering data $("#accounts-select").select2();
+
+    return this;
+  }
+});
