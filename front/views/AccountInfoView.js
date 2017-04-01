6c10bad31b65d7d3646ac5af36fc20e7be8eb4e3
betomtz <luis.mave88@gmail.com>
2017-04-01 12:43:55 -0600

    Add resource and fix groupstemplate

--- front/views/AccountInfoView.js
+++ front/views/AccountInfoView.js
@@ -4,6 +4,25 @@ let AccountInfoView = Backbone.View.extend({
     tagName:"div",
     className:"row",
 
+/*    initialize: () =>{
+        Semitki.users.fetch(Semitki.addAuthorizationHead());
+    },*/
+    
+    
+    events: {
+    
+    "click #save": "save"
+},
+    
+    save: () => {
+        
+        let data = {
+            username: $("#name").val()
+            
+        };
+        alert("fuck");
+    },
+    
   render: function() {
     let template = $("#accountinfo").html();
     let compiled = Handlebars.compile(template);
