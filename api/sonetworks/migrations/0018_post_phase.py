e71fb53b579a461ace543ce5aba46a62358c9c31
gvaldez81 <gvaldez@gmail.com>
2017-04-11 16:19:53 -0500

    Fix for migration 0018 in case of posts table having already records

--- api/sonetworks/migrations/0018_post_phase.py
+++ api/sonetworks/migrations/0018_post_phase.py
@@ -7,6 +7,15 @@ from django.db import migrations, models
 import django.db.models.deletion
 from django.utils.timezone import utc
 
+def update_tables():
+    from sonetworks.settings import PROJECT_DIR
+    import os
+    sql_statements = open(os.path.join(PROJECT_DIR,'sql/posts.sql'), 'r').read()
+    return sql_statements
+
+def get_uuid(apps, schema_editor):
+    modelRef = apps.get_model('sonetworks', 'Phase')
+    return modelRef.objects.all()[0].id
 
 class Migration(migrations.Migration):
 
@@ -15,10 +24,16 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunSQL(update_tables()),
+    ], 
+    
+    migrations.RunPython(get_uuid),
+
+    operations = [
         migrations.AddField(
             model_name='post',
             name='phase',
-            field=models.ForeignKey(to='sonetworks.Phase'),
+            field=models.ForeignKey(to='sonetworks.Phase', default=get_uuid),
             preserve_default=False,
         ),
     ]
