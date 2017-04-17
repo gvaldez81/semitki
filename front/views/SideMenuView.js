46945b3239909f4cd5a7f498252fe1106266d73d
Ernesto Celis <developer@celisdelafuente.net>
2017-04-17 18:58:59 -0500

    WIP side menu, it renders. Althought it is still hidden

---
+++ front/views/SideMenuView.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let SideMenu = Backbone.View.extend({
+
+
+  render: function() {
+    let template = $("#side-menu-template").html();
+    let compiled = Handlebars.compile(template);
+    this.$el.html(compiled());
+    $("#side-menu").html(this.$el);
+
+    return this;
+  }
+});
