c74637b2531867c27d7e278b0e5cba1a03c2ac8b
gvaldez81 <gvaldez@gmail.com>
2017-04-21 11:26:51 -0500

    Accounts groupped, views and templates

--- front/views/GrouppedAccountsView.js
+++ front/views/GrouppedAccountsView.js
@@ -6,30 +6,34 @@ let GrouppedAccountsView = Backbone.View.extend({
   
   initialize: function(){
     this.navigation = new NavigationView();
+    this.relatedaccount = new GrouppedAccountsRelatedView();
     this.footer = new FooterView();    
   },
   
   events: {
-    "draggable #relateaccount": "draggable",
+    "change #groups": "filterGroup",
   },
 
-draggable: () =>{
-  
-},
+  filterGroup: () =>{
+    S.collection.get("account_groups").filtering($('#groups').val())
+    let view = new GrouppedAccountsRelatedView();
+    view.render();
+  },
 
   render: function() {
     
-let data = {
-  groups: S.collection.get("groups").toJSON(),
-  account_groups: S.collection.get("account_groups").toJSON(),
-  accounts: S.collection.get("accounts").toJSON()  
+  let data = {
+    groups: S.collection.get("groups").toJSON(),
+    account_groups: S.collection.get("account_groups").toJSON(),
+    accounts: S.collection.get("accounts").toJSON()  
 
-};
+  };
             
   let template = $("#groupped-account").html();
   let compiled = Handlebars.compile(template);
   this.$el.html(compiled(data));
-  $("#main").html(this.$el);   
+  $("#main").html(this.$el);  
+  this.relatedaccount.render();
 
   return this;    
   }
