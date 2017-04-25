a4e5ba1c578ac653c7a9e7fc1be4426709906513
betomtz <luis.mave88@gmail.com>
2017-04-25 11:54:17 -0500

    fix filter related

--- front/views/GrouppedAccountsView.js
+++ front/views/GrouppedAccountsView.js
@@ -1,51 +1,47 @@
 'use strict'
 
 let GrouppedAccountsView = Backbone.View.extend({
-  tagName:"div",
+  tagName: "div",
   className: "row",
-  
-  initialize: function(){
+
+  initialize: function () {
     this.navigation = new NavigationView();
     this.relatedaccount = new GrouppedAccountsRelatedView();
-    this.footer = new FooterView();    
-  },
-  
-  events: {
-    "change #groups": "filterACGroup",
+    this.footer = new FooterView();
   },
 
-  filterACGroup: () =>{
+  events: {
+    "change #groups": "filterGroup",
+  },
+
+  filterACGroup: () => {
     S.collection.get("account_groups").filtering($('#groups').val())
     let view = new GrouppedAccountsRelatedView();
     view.render();
   },
 
-  filterGroup: () =>{
+  filterGroup: () => {
     S.collection.get("groups").filtering($('#groups').val())
     let view = new GrouppedAccountsRelatedView();
     view.render();
   },
 
 
-  render: function() {
-    
-  let data = {
-    groups: S.collection.get("groups").toJSON(),
-    account_groups: S.collection.get("account_groups").toJSON(),
-    accounts: S.collection.get("accounts").toJSON()  
+  render: function () {
 
-  };
-            
-  let template = $("#groupped-account").html();
-  let compiled = Handlebars.compile(template);
-  this.$el.html(compiled(data));
-  $("#main").html(this.$el);  
-  this.relatedaccount.render();
-  // Initialization
-  $("#groups").select2({
-      placeholder: 'Select a Group',
-      minimumResultsForSearch: 20  //Infinity
-  });
-  return this;    
+    let data = {
+      groups: S.collection.get("groups").toJSON(),
+      account_groups: S.collection.get("account_groups").toJSON(),
+      accounts: S.collection.get("accounts").toJSON()
+
+    };
+
+    let template = $("#groupped-account").html();
+    let compiled = Handlebars.compile(template);
+    this.$el.html(compiled(data));
+    $("#main").html(this.$el);
+    this.relatedaccount.render();
+
+    return this;
   }
 });
