3ddca3eca74c1a8a300da606bc621d9f9463d510
betomtz <luis.mave88@gmail.com>
2017-04-26 11:59:41 -0500

    create template groupped account

--- front/views/GrouppedAccountsRelatedView.js
+++ front/views/GrouppedAccountsRelatedView.js
@@ -1,41 +1,41 @@
 'use strict'
 
 let GrouppedAccountsRelatedView = Backbone.View.extend({
-  tagName:"div",
+  tagName: "div",
   className: "col-md-6 col-md-offset-3",
-  
-  initialize: function(){
+
+  initialize: function () {
     this.navigation = new NavigationView();
-    this.footer = new FooterView();    
+    this.footer = new FooterView();
   },
-  
 
 
-  render: function() {
+
+  render: function () {
     let data = {
       groups: S.collection.get("groups").toJSON()
     };
-    if (S.collection.get("related")!== undefined){
-      data.groupped= S.collection.get("related").toJSON()
-      
+    if (S.collection.get("related") !== undefined) {
+      data.groupped = S.collection.get("related").toJSON()
+
     }
-    Handlebars.registerHelper('lookup2', function(collection, id) {
+    Handlebars.registerHelper('lookup2', function (collection, id) {
       var collectionLength = collection.length;
 
       for (var i = 0; i < collectionLength; i++) {
-          if (collection[i].id === id) {
-              return collection[i];
-          }
+        if (collection[i].id === id) {
+          return collection[i];
+        }
 
       }
 
       return null;
-    });      
+    });
     let template = $("#grouppedaccount-related").html();
     let compiled = Handlebars.compile(template);
     this.$el.html(compiled(data));
-    $("#relateaccount").html(this.$el);   
-    return this;      
-    
+    $("#relateaccount").html(this.$el);
+    return this;
+
   }
 });
