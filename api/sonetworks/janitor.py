6f6fa29a7a03d011b5cd9dd666b0939376da74c1
Ernesto Celis <developer@celisdelafuente.net>
2017-05-08 17:39:27 -0500

    WIP janitor for publishing right away when creating post

--- api/sonetworks/janitor.py
+++ api/sonetworks/janitor.py
@@ -29,30 +29,3 @@ class ServiceAccount:
     def __init__(self, account_id):
         pass
 
-
-
-class OAuthDance:
-
-    def handle_callback(self, code=None, token=None):
-        if(code != None):
-            payload = {'client_id': settings.SOCIAL_AUTH_FACEBOOK_KEY,
-                    'redirect_uri': 'http://localhost:8000/callback/',
-                    'client_secret': settings.SOCIAL_AUTH_FACEBOOK_SECRET,
-                    'code': code}
-            r = requests.get('https://graph.facebook.com/v2.9/oauth/access_token',
-                    params=payload)
-            return r.json()
-
-        if(token != None):
-            account = SocialAccount(
-                    username="123",
-                    email="email",
-                    password=None,
-                    access_token=token,
-                    bucket="facebook"
-                    )
-            account.save()
-            return "Algo"
-
-        if(code == None and token == None):
-            return "All empty"
