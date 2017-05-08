a43188d4a6ae6522b2eb2d4287f29280eeb49e42
gvaldez81 <gvaldez@gmail.com>
2017-05-08 15:34:43 -0500

    Meta tags for TwittCards

--- front/views/LandingPage.js
+++ front/views/LandingPage.js
@@ -30,6 +30,15 @@ let LandingPageView = Backbone.View.extend({
 
 
   render: function() {
+    var head = $('head')
+    h.append('<meta name="twitter:card" content="summary_large_image">')
+    h.append('<meta name="twitter:site" content="@SirGerri">')
+    h.append('<meta name="twitter:creator" content="@MexicoEligeBien">')
+    h.append('<meta name="twitter:title" content="Mexico, es momento de elegir, #ElegirBien">')
+    h.append('<meta name="twitter:description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu finibus metus, ut eleifend odio. Praesent a ligula bibendum ex pulvinar tempor.">')
+    h.append('<meta name="twitter:image" content="http://159.203.134.236:9080/img/background.png">')
+
+
     let template = $("#landing-template").html();
     let compiled = Handlebars.compile(template);
     this.$el.html(compiled);
