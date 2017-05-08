f95d8e98072622e5c0babc3d481c0e3ddb9d1a4c
gvaldez81 <gvaldez@gmail.com>
2017-05-08 15:44:01 -0500

    Fix Meta tags for TwittCards

--- front/views/LandingPage.js
+++ front/views/LandingPage.js
@@ -31,12 +31,12 @@ let LandingPageView = Backbone.View.extend({
 
   render: function() {
     var head = $('head')
-    h.append('<meta name="twitter:card" content="summary_large_image">')
-    h.append('<meta name="twitter:site" content="@SirGerri">')
-    h.append('<meta name="twitter:creator" content="@MexicoEligeBien">')
-    h.append('<meta name="twitter:title" content="Mexico, es momento de elegir, #ElegirBien">')
-    h.append('<meta name="twitter:description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu finibus metus, ut eleifend odio. Praesent a ligula bibendum ex pulvinar tempor.">')
-    h.append('<meta name="twitter:image" content="http://159.203.134.236:9080/img/background.png">')
+    head.append('<meta name="twitter:card" content="summary_large_image">')
+    head.append('<meta name="twitter:site" content="@SirGerri">')
+    head.append('<meta name="twitter:creator" content="@MexicoEligeBien">')
+    head.append('<meta name="twitter:title" content="Mexico, es momento de elegir, #ElegirBien">')
+    head.append('<meta name="twitter:description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu finibus metus, ut eleifend odio. Praesent a ligula bibendum ex pulvinar tempor.">')
+    head.append('<meta name="twitter:image" content="http://159.203.134.236:9080/img/background.png">')
 
 
     let template = $("#landing-template").html();
