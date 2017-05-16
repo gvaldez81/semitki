a10b4c3b2c5e5a76e2280b5b9afa0d1f5755fdbd
lmartinez <luis.mave88@gmail.com>
2017-05-15 20:39:03 -0500

    Fix events save, edit and delete in userviews

--- front/views/addUserView.js
+++ front/views/addUserView.js
@@ -3,20 +3,52 @@
 let addUserView = Backbone.View.extend({
   tagName: "div",
 
-
   className: "modal-dialog",
 
     initialize: function(data) {
     this.data = data || undefined;
+
   },
 
   events: {
-    "click #save": "saveUser"
+    "click #save": "saveuser"
   },
 
-   saveUser:() =>{
+  saveuser: function(e) { 
 
-    let data = {
+    e.preventDefault();
+    let options = {
+
+      error: (error) => {
+
+        $('#dialog-crud').modal('hide');     
+        S.logger("bg-danger", "Couldn't User Save", true);
+
+      },
+
+      success: (model, reponse) => {
+
+        console.log(model);
+        $('#dialog-crud').modal('hide');       
+        let userView = new UserView();
+        userView.render();
+        S.logger("bg-success", "Save User Succesfully", true);
+
+      },
+
+        wait: true,
+        headers: S.addAuthorizationHeader().headers,
+    }
+
+    let group = S.collection.get("user")
+        .create(this.adduser(), options);
+        console.log("User");
+
+  },
+
+  adduser:() =>{
+
+    let user = {
 
       first_name: $("#input_fname").val(),
       last_name: $("#input_lname").val(),
@@ -24,35 +56,18 @@ let addUserView = Backbone.View.extend({
       username: $("#input_username").val()
     };
 
-    let user = new UserModel(data);
-    S.collection.get("user")
-      .add(user)
-      .sync("create", user, S.addAuthorizationHeader());
-    console.log("saveuser")
-    //Cerramos modal
-    $('#dialog-crud').modal('hide')
-    //Abrimos modal de success
-    bootbox.alert({
-      message: "User saved",
-      size: 'small',
-      className: 'rubberBand animated'
+    let users = new UserModel(user);
+    return users;
 
-    });
-    
-    let userView = new UserView();
-        userView.render();
   },
 
-
-
-
-
   render: function(){
+
     let template = $("#user-modal-add").html();
     let compiled = Handlebars.compile(template);
     this.$el.html(compiled(this.data));
     $("#dialog-crud").html(this.$el);
-  },
 
+  },
 
 });
