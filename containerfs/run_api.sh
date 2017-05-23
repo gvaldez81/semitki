77a4f66223e1182fe822cd106516b30c9d41a631
Ernesto Celis <developer@celisdelafuente.net>
2017-05-23 15:07:25 -0500

    FIX typo

--- containerfs/run_api.sh
+++ containerfs/run_api.sh
@@ -4,6 +4,6 @@ ENV/bin/pip install -r requirements.txt
 cd /semitki/api
 /semitki/ENV/bin/python /semitki/api/manage.py migrate --noinput
 ## uWSGI production app server
-/semitki/ENV/bin/uwsgi --ini /semitki/config/emperor.ini
+/semitki/ENV/bin/newrelic-admin run-program /semitki/ENV/bin/uwsgi --ini /semitki/config/emperor.ini
 ## Django development server
 /semitki/ENV/bin/python /semitki/api/manage.py runserver 0.0.0.0:8000
