240d2a3cc58f7ece0b816ef6f0407680837ef409
Ernesto Celis <developer@celisdelafuente.net>
2017-05-23 14:40:27 -0500

    UPDATE to work with uWSGI

--- containerfs/run_api.sh
+++ containerfs/run_api.sh
@@ -3,7 +3,7 @@ cd /semitki
 ENV/bin/pip install -r requirements.txt
 cd /semitki/api
 /semitki/ENV/bin/python /semitki/api/manage.py migrate --noinput
-## Django development server
-# /semitki/ENV/bin/python /semitki/api/manage.py runserver 0.0.0.0:8000
 ## uWSGI production app server
-/semitki/ENV/bin/uwsgi /semitki/emperor.ini
+/semitki/ENV/bin/uwsgi --ini /semitki/config/emperor.ini
+## Django development server
+/semitki/ENV/bin/python /semitki/api/manage.py runserver 0.0.0.0:8000
