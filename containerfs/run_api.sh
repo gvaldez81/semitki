62791f5ba25d99b3a439295639524bf63ae29c6d
Ernesto Celis <developer@celisdelafuente.net>
2017-05-24 17:06:01 -0500

    UPDATE new envirnoment variable SEMITKI_ENV which can be set to develepment ot production values, currently only launches newrelic agent in production

--- containerfs/run_api.sh
+++ containerfs/run_api.sh
@@ -4,6 +4,10 @@ ENV/bin/pip install -r requirements.txt
 cd /semitki/api
 /semitki/ENV/bin/python /semitki/api/manage.py migrate --noinput
 ## uWSGI production app server
-/semitki/ENV/bin/newrelic-admin run-program /semitki/ENV/bin/uwsgi --ini /semitki/config/emperor.ini
+if [ "${SEMITKI_ENV}" = "production" ]; then
+  /semitki/ENV/bin/newrelic-admin run-program /semitki/ENV/bin/uwsgi --ini /semitki/config/emperor.ini
+else
+  /semitki/ENV/bin/uwsgi --ini /semitki/config/emperor.ini
+fi
 ## Django development server
 /semitki/ENV/bin/python /semitki/api/manage.py runserver 0.0.0.0:8000
