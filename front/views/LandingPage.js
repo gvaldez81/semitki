fd7a301ea473ad9b691695c3280a67930ed56344
gvaldez81 <gvaldez@gmail.com>
2017-05-26 13:22:10 -0500

    TwittCard for MexicoEligeBien

--- front/views/LandingPage.js
+++ front/views/LandingPage.js
@@ -42,12 +42,11 @@ let LandingPageView = Backbone.View.extend({
   render: function() {
     var head = $('head')
     head.append('<meta name="twitter:card" content="summary_large_image">')
-    head.append('<meta name="twitter:site" content="@SirGerri">')
-    head.append('<meta name="twitter:creator" content="@MexicoEligeBien">')
+    head.append('<meta name="twitter:site" content="@MexicoEligeBien">')
+    head.append('<meta name="twitter:creator" content="@SirGerri">')
     head.append('<meta name="twitter:title" content="Mexico, es momento de elegir, #ElegirBien">')
     head.append('<meta name="twitter:description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu finibus metus, ut eleifend odio. Praesent a ligula bibendum ex pulvinar tempor.">')
-    head.append('<meta name="twitter:image" content="http://159.203.134.236:9080/img/background.png">')
-
+    head.append('<meta property="og:image" content="http://mexicoeligebien.mx/img/background.png">')
 
     let template = $("#landing-template").html();
     let compiled = Handlebars.compile(template);
