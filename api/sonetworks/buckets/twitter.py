5e1d6d542148585923ed0f95a7aa484be1b16c66
gvaldez81 <gvaldez@gmail.com>
2017-06-08 18:24:52 -0500

    Post ID in Twitter Fav 4

--- api/sonetworks/buckets/twitter.py
+++ api/sonetworks/buckets/twitter.py
@@ -17,17 +17,6 @@ from urlparse import urlparse
 import tweepy
 
 
-def get_post_id(permalink):
-        """
-        Returns a Twitter Post Id from permalink
-        """
-        path = urlparse(permalink).path
-        if path.endswith('/'):
-            path = path[:len(path)-1]
-        k = path.rfind("/")
-        return path[k+1:]
-
-
 class Twitter:
 
     def __init__(self, account_id = None):
@@ -40,7 +29,15 @@ class Twitter:
         self.oauth = None
         self.url = 'https://twitter.com/'
 
-    
+    def get_post_id(self, permalink):
+        """
+        Returns a Twitter Post Id from permalink
+        """
+        path = urlparse(permalink).path
+        if path.endswith('/'):
+            path = path[:len(path)-1]
+        k = path.rfind("/")
+        return path[k+1:]
 
     def fav(self, token, permalink, account_id, post_id):
         """Like an existing tweet given the tweet id"""
@@ -49,7 +46,7 @@ class Twitter:
         api = tweepy.API(auth)
 
         if post_id == 0:
-            post_id = get_post_id(permalink)
+            post_id = self.get_post_id(permalink)
 
         try:
             rt = api.create_favorite(id=post_id)
